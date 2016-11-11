01de8b86deae7c10f577de3a61b18c6f690b3a05
pipav <pipav@snoop>
2016-11-11 15:04:14 +0100

    sample documents and generated document's folders added.

--- DocumentGenerator.py
+++ DocumentGenerator.py
@@ -7,14 +7,19 @@ import random
 
 i = 0
 # Set here the number of records you need
-j = 1000
+j = int(input('How much documents do you want to create?'))
 # Set here the wiche file name's length
 length = 30
+
+docURI1 = 'sample_documents/doc1'
+docURI2 = 'sample_documents/doc2'
+docChoice = [docURI1, docURI2]
 
 while i < j :
 	newFileName=''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase+string.digits) 
 	for _ in range(length))
-	shutil.copyfile('sample_documents/document', 'generated_documents/'+newFileName)
-	recordNumber = str(i)
+	selectedDoc = random.choice(docChoice)
+	shutil.copyfile(selectedDoc, 'generated_documents/'+newFileName)
+	recordNumber = str(i+1)
 	print ('Record '+recordNumber+' - file ' +newFileName+' generated')
 	i = i+1
